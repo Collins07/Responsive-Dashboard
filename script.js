276b5474e43c6130f420458f249f203a193c4a9b
Collins07 <collinsabaya07@gmail.com>
2022-09-19 23:23:35 +0300

    menu toggle JS

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function activelink(){
 }
 list.forEach((item) => 
 item.addEventListener('mouseover', activelink));
+
+// menu toggle
+
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function(){
+    navigation.classList.toggle('active')
+}
